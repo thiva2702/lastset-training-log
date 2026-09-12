b9f83c707336588734fa85a306504910c25adfa6
thiva2702 <125878550+thiva2702@users.noreply.github.com>
2026-09-13 03:44:24 +0800

    Fix Smart Log typo handling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'lastset-v011-cardio-bodyweight';
+const CACHE = 'lastset-v0111-typo-fix';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
